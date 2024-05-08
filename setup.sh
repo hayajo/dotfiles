e83e80185c97c5627541dd0cfc2c79879ac85064
hayajo <848587+hayajo@users.noreply.github.com>
2024-05-08 16:10:10 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ ABSPATH="$(cd "$(dirname $0)"; pwd)"
 find "$ABSPATH" \
   -mindepth 1 \
   -maxdepth 1 \
-  -not \( -name README.md -o -name .git \) \
+  -not \( -name README.md -o -name setup.sh -o -name .git \) \
   -exec ln -sf {} "$HOME" \;
