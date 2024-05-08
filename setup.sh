f3a791f42160cec11ddb7c78e1e483f0c5ea821b
hayajo <848587+hayajo@users.noreply.github.com>
2024-05-08 14:53:37 +0900

    refactor setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-ABSPATH=$(cd "$(dirname $0)"; pwd)
+ABSPATH="$(cd "$(dirname $0)"; pwd)"
 
-find $ABSPATH \
-  -mindepth 1 -maxdepth 1 \
-  -not \( -name README.md -o -name .git -o -name .gitmodules -o -name $(basename $0) \) \
-  | xargs -I@ ln -sf @ $HOME
+find "$ABSPATH" \
+  -mindepth 1 \
+  -maxdepth 1 \
+  -not \( -name README.md -o -name .git \) \
+  -exec ln -sf {} "$HOME" \;
