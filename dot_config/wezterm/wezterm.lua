5ded5c12d4f3908f9ce916ae10bb4702b0161c32
hayajo <848587+hayajo@users.noreply.github.com>
2025-03-19 11:35:32 +0900

    windwow_padding: 0 in wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,7 +2,14 @@ local wezterm = require 'wezterm'
 local config  = wezterm.config_builder()
 local act     = wezterm.action
 
-config.window_background_opacity    = 0.88
+config.window_padding = {
+  left   = 0,
+  right  = 0,
+  top    = 0,
+  bottom = 0,
+}
+
+-- config.window_background_opacity    = 0.88
 -- config.macos_window_background_blur = 20
 
 -- config.font                      = wezterm.font("Monaco")
