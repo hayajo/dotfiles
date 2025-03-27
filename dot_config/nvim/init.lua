8819ac3086858dd0f9a8db992606c4e80dac5a73
hayajo <848587+hayajo@users.noreply.github.com>
2025-03-27 09:41:42 +0900

    set netrw to avoid prioritizing sorting of files that have permission to execute

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -168,6 +168,8 @@ vim.opt.wildmode      = "longest:full,full"
 vim.opt.termguicolors = true
 vim.opt.winblend      = 0 -- ウィンドウの不透明度
 vim.opt.pumblend      = 0 -- ポップアップメニューの不透明度
+
+vim.g.netrw_sort_sequence = "[\\/]$,*" -- netrw で実行権のあるファイルを先頭に表示させない
 -- }}}
 
 -- Key Mappings {{{
